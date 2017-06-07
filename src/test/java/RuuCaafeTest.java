a783e89e5c0f735cd28934e0c237feed196a9e81
JS <jamez.bender@gmail.com>
2017-06-07 21:05:36 +0300

    Initial commit

---
+++ src/test/java/RuuCaafeTest.java
@@ -0,0 +1,68 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+
+
+public class RuuCaafeTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "D://chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void titleNameIsCorrect() {
+        driver.get("http://ru-cafe.ru/");
+        String title = driver.getTitle();
+        Assert.assertEquals("Круглосуточный копировальный центр в Москве. Оперативная полиграфия.", title);
+
+    }
+
+
+    @Test
+    public void shouldBeInvalidLoginMessage() {
+        driver.get("http://ru-cafe.ru/account/login");
+        driver.findElement(By.xpath(".//*[@id='ctl00_cphMainPanel_ctl00_ctl00_txtLogin']")).sendKeys("asdasdasd");
+        driver.findElement(By.xpath(".//*[@id='ctl00_cphMainPanel_ctl00_ctl00_txtPassword']")).sendKeys("qweqweqwe");
+        driver.findElement(By.xpath(".//*[@id='ctl00_cphMainPanel_ctl00_ctl00_butLogin']")).click();
+        String err = driver.findElement(By.xpath(".//*[@id='aspnetForm']/div[3]/div/div/div[1]/fieldset/div")).getText();
+        Assert.assertEquals("Вход не удался: эл. почта или пароль не верен", err);
+    }
+
+    @Test
+    public void shouldBeFailedInvalidLoginMessage() {
+        driver.get("http://ru-cafe.ru/account/login");
+        driver.findElement(By.id("ctl00_cphMainPanel_ctl00_ctl00_txtLogin")).sendKeys("asdasdasd");
+        driver.findElement(By.id("ctl00_cphMainPanel_ctl00_ctl00_txtPassword")).sendKeys("qweqweqwe");
+        driver.findElement(By.id("ctl00_cphMainPanel_ctl00_ctl00_butLogin")).click();
+        String err = driver.findElement(By.xpath("//form[@id='aspnetForm']/div[3]/div/div/div/fieldset/div")).getText();
+        Assert.assertEquals("Вход не удался", err);
+
+    }
+
+    @Test
+    public void shouldBeInvalidRestoreMessage() {
+        driver.get("http://ru-cafe.ru/account/restore");
+        driver.findElement(By.xpath(".//*[@id='ctl00_cphMainPanel_txtEmail']")).sendKeys("user");
+        driver.findElement(By.xpath(".//*[@id='ctl00_cphMainPanel_butRestore']")).click();
+        String err = driver.findElement(By.xpath(".//*[@id='ctl00_cphMainPanel_exprEmail']")).getText();
+        Assert.assertEquals("Введено неверное значение", err);
+    }
+
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        driver.close();
+    }
+
+
+}
+
